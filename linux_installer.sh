f4032d5f16c03a9994949f9f448ae545dc9b03aa
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-04-13 14:57:01 +0700

    add linux installer shell

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,4 +1,5 @@
 mkdir $HOME/.control
-curl https://raw.githubusercontent.com/Arikato111/control/master/control >> $HOME/.control/control
-curl 
-echo "export PATH=$PATH:$HOME/.control"
+curl https://raw.githubusercontent.com/Arikato111/control/ltp/control >> $HOME/.control/control
+curl https://raw.githubusercontent.com/Arikato111/control/ltp/ltp >> $HOME/.control/ltp
+echo "export PATH=\$PATH:$HOME/.control" >> $HOME/.bashrc
+sudo chmod +x $HOME/.control/ltp
